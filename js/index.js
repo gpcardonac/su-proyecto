fefdd47bb06891e543af700396b96565252041bb
Patricia Cardona <gpcardonac@gmail.com>
2023-02-26 21:45:28 -0500

    Se agregan dos botones captura de datos de un estudiante cel y nombre

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function captura(){
+    var nombreest=document.getElementById("nombreest").value;
+    var nombreest=document.getElementById("celest").value;
+    console.log(nombreest +""+celest);
+
+}
