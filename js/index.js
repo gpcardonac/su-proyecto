8188d27140ad0ca8bb6e2c9e1f451fbd92dd4b07
Patricia Cardona <gpcardonac@gmail.com>
2023-02-27 16:11:09 -0500

    Se comenta en archivo index.js el modo de captura de datos en dos botones en la pagina web generada con index.html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-//capturar texto en un cuadro
+//capturar texto en un formulario.  La captura de datos se observa en 
+//la consola pulsando F12.  Si se borra y se agrega otro nombre y numero
+//de teléfono, en la consola se observa que se guardan los datos a capturar.
 
 function captura(){
     var nombreest=document.getElementById("nombreest").value;
